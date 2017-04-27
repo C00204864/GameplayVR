e152f7a20233cbc356d407b544a7a8fc63ff6721
(Student) - Liam Hickey <c00204864@ITCARLOW.IE>
2017-04-27 14:40:32 +0100

    Project Commit

---
+++ Virtual Reality Game/Assets/GoalCollider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoalCollider : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        int levelNo = 0;
+        if (collision.gameObject.name == "Sphere")
+        {
+            string sceneName = SceneManager.GetActiveScene().name;
+            char last = sceneName[sceneName.Length - 1];
+            levelNo = last - 48;
+            levelNo++;
+            if (4 == levelNo)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene("level" + levelNo);
+            }
+        }
+    }
+}
